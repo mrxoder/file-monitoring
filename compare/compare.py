836c87d8f9bafd52e9e127b6e7438b8568b463c0
xoder <xoderxoderx@gmail.com>
2024-02-21 01:56:16 -0500

    fixing

--- compare/compare.py
+++ compare/compare.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 
 class Compare:
@@ -11,10 +12,29 @@ class Compare:
         self._deleted =[]
 
     def check( self ):
-        json_0 = json.load( open(self.file_0) ) 
-        json_1 = json.load( open(self.file_1) )
+        try:
+            json_0 = json.load( open(self.file_0) ) 
+            json_1 = json.load( open(self.file_1) )
+        except:
+            print("Bad json.")
 
-        for _file in json_1:
-            if not json_0[ ]
+        for _file in json_1["files"]:
+            if not _file in json_0["files"].keys():
+                self._created.append( _file )
+            else:
+                if json_0["files"][ _file ] != json_1["files"][ _file ]:
+                    self._changed.append( _file )
+
+        for _file in json_0["files"]:
+            if not _file in json_1["files"].keys():
+                self._deleted.append( _file )
+
+
+
+if len(sys.argv) < 3:
+    print("\n Usage: "+sys.argv[0]+" <oldest json scan result>  <newest json scan result>")
+else:
+    comparison = Compare( sys.argv[1], sys.argv[2] )  
+            
 
         
