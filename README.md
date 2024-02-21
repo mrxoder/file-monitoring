a629d29470782e16543ebc09b22c45f9b4c38608
xoder <xoderxoderx@gmail.com>
2024-02-21 02:59:38 -0500

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+- Generate gpg key and add the public key to `GPGPublicKey.asc`
+- Upload to php server:
+    
--- scan.php
+++ scan.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require_once("./Scanner.php");
 
 $basePath = "./"; 
@@ -8,4 +7,4 @@ $scanner = new Scanner( $basePath );
 $hashes = $scanner->scan();
 $date = date("h_i_s-d_M_Y");
 
-$scanner->Save( "scan_$date.dat", [ "files"=>$hashes, "basepath"=>$basePath, "date"=>$date ] );
+$scanner->Save( "_scan_result/scan_$date.dat", [ "files"=>$hashes, "basepath"=>$basePath, "date"=>$date ] );
